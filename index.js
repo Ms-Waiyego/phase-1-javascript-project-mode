134e41e84a9179b2d61ffc0e831301bd243fe174
Ms-Waiyego <waiyegobrenda99@gmail.com>
2022-09-01 21:08:44 +0300

    add event listener 1

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-document.addEventListener('DOMContentLoaded',() => {
-    const myForm = document.querySelector('form')
-    myForm.addEventListener('submit', onSubmit)
-   function onSubmit(event) {
-       event.preventDefault();
-      const comment =document.getElementById('comment').value
-      //return (`${comment}`)
-     console.log (comment)
-   }
-        console.log('After DOM loaded')
-   });
-    console.log('Before DOM loads')
+//Creating a submit button
+//This button lets one submit thier comment on the verse given
 
-    
+document.addEventListener("DOMContentLoaded", () => {
+const form = document.querySelector('form')
+form.addEventListener('submit', onSubmit);
+function onSubmit(e) {
+  e.preventDefault();
+  handleComment(e.target["add-comment"].value)
+}
+function handleComment(comment) {
+  let p = document.createElement('p')
+  p.textContent = comment
+  console.log(p)
+ document.querySelector("#comment").appendChild(p)
+
+}
+  console.log ("After DOM loaded")
+});
+console.log('Before DOM loads')
+
+//Getting the API that will generate the bible verse need 
+    let adviceField = document.getElementById('verse')
+    fetch(`https://bible-api.com/john%203:16`)
+  .then((response) => response.json())
+  .then((verse) => console.log(verse))
+
+
+  function showVerse(verse){
+    const bibleVerse =document.querySelector('#vesre')
+    const versePlace =document.createElement("p")
+    document.appendChild('p')
+
+  }
+   
+  
