8c80a8f627a05f561826335de722275f54379f95
Ms-Waiyego <waiyegobrenda99@gmail.com>
2022-09-02 18:46:09 +0300

    completion on working on fetch

--- index.js
+++ index.js
@@ -17,6 +17,10 @@ function handleComment(comment) {
   console.log ("After DOM loaded")
 });
 console.log('Before DOM loads')
+
+//Creating a function that will fetch the API data 
+//Then convert them to the required form
+//Then post them of the webpage when the button get verse is pressed 
 document.getElementById('getverse').addEventListener('click', getverse)
 function getverse(verse) {
   fetch(`https://bible-api.com/romans+12:1-2`,{
@@ -30,6 +34,7 @@ function getverse(verse) {
    document.getElementById('verse').innerHTML= JSON.stringify(bibleverse.text)
   } )
   }
+
   
 
 
